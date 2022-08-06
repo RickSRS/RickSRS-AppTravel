14888a6aeadce5c283be18ad2283b7b8d8072150
Richard Soares <soaresr870@gmail.com>
2022-08-06 17:18:36 -0300

    binding package resume

--- app/src/main/java/com/richardsoares/apptravel/ui/activity/ResumoPacoteActivity.java
+++ app/src/main/java/com/richardsoares/apptravel/ui/activity/ResumoPacoteActivity.java
@@ -2,9 +2,19 @@ package com.richardsoares.apptravel.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.richardsoares.apptravel.R;
+import com.richardsoares.apptravel.model.Pacote;
+import com.richardsoares.apptravel.util.DiasUtil;
+import com.richardsoares.apptravel.util.MoedaUtil;
+import com.richardsoares.apptravel.util.ResourceUtil;
+
+import java.math.BigDecimal;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
@@ -12,5 +22,22 @@ public class ResumoPacoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        TextView local = findViewById(R.id.resumo_pacote_local);
+        local.setText(pacoteSaoPaulo.getLocal());
+
+        ImageView imagem = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawablePacote = ResourceUtil.devolveDrawable(this, pacoteSaoPaulo.getImagem());
+        imagem.setImageDrawable(drawablePacote);
+
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
+        String diasEmTexto = DiasUtil.formataEmTexto(pacoteSaoPaulo.getDias());
+        dias.setText(diasEmTexto);
+
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
+        String moedaBrasileira = MoedaUtil.formataMoedaParaBrasileiro(pacoteSaoPaulo.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
