791a99cf938dd0303433b8822c09738764e59aac
Richard Soares <soaresr870@gmail.com>
2022-08-06 19:14:59 -0300

    bind activity pay and refactoring

--- app/src/main/java/com/richardsoares/apptravel/ui/activity/PagamentoActivity.java
+++ app/src/main/java/com/richardsoares/apptravel/ui/activity/PagamentoActivity.java
@@ -3,14 +3,32 @@ package com.richardsoares.apptravel.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.richardsoares.apptravel.R;
+import com.richardsoares.apptravel.model.Pacote;
+import com.richardsoares.apptravel.util.MoedaUtil;
+
+import java.math.BigDecimal;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacote = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        exibePreco(pacote);
+    }
+
+    private void exibePreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_preco);
+        String moedaBrasileira = MoedaUtil.formataMoedaParaBrasileiro(pacote.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
