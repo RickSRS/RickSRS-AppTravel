ad5c7da5080ec12e97f1c410e43a482378fb1b78
Richard Soares <soaresr870@gmail.com>
2022-08-07 00:56:07 -0300

    code refactor

--- app/src/main/java/com/richardsoares/apptravel/ui/activity/ResumoCompraActivity.java
+++ app/src/main/java/com/richardsoares/apptravel/ui/activity/ResumoCompraActivity.java
@@ -1,11 +1,11 @@
 package com.richardsoares.apptravel.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.richardsoares.apptravel.R;
 import com.richardsoares.apptravel.model.Pacote;
@@ -13,34 +13,47 @@ import com.richardsoares.apptravel.util.DataUtil;
 import com.richardsoares.apptravel.util.MoedaUtil;
 import com.richardsoares.apptravel.util.ResourceUtil;
 
-import org.w3c.dom.Text;
-
 import java.math.BigDecimal;
 
 public class ResumoCompraActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Resumo da Compra";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_compra);
 
-        setTitle("Resumo da Compra");
+        setTitle(TITULO_APPBAR);
 
         Pacote pacote = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
 
-        TextView local = findViewById(R.id.resumo_compra_local);
-        local.setText(pacote.getLocal());
+        exibeLocal(pacote);
+        exibeImagem(pacote);
+        exibeData(pacote);
+        exibePreco(pacote);
+    }
 
-        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
-        Drawable drawablePacote = ResourceUtil.devolveDrawable(this, pacote.getImagem());
-        imagem.setImageDrawable(drawablePacote);
-
-        TextView data = findViewById(R.id.resumo_compra_data);
-        String periodoEmTexto = DataUtil.PeriodoEmTexto(pacote.getDias());
-        data.setText(periodoEmTexto);
-
+    private void exibePreco(Pacote pacote) {
         TextView preco = findViewById(R.id.resumo_compra_preco);
         String moedaBrasileira = MoedaUtil.formataMoedaParaBrasileiro(pacote.getPreco());
         preco.setText(moedaBrasileira);
+    }
+
+    private void exibeData(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_compra_data);
+        String periodoEmTexto = DataUtil.PeriodoEmTexto(pacote.getDias());
+        data.setText(periodoEmTexto);
+    }
+
+    private void exibeImagem(Pacote pacote) {
+        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
+        Drawable drawablePacote = ResourceUtil.devolveDrawable(this, pacote.getImagem());
+        imagem.setImageDrawable(drawablePacote);
+    }
+
+    private void exibeLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_compra_local);
+        local.setText(pacote.getLocal());
     }
 }
